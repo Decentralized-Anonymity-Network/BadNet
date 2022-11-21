77c038b315458b1693931b948cd3d299e9fd2f15
hhyyang <87637701+hhyyang@users.noreply.github.com>
2022-11-21 14:06:42 +0800

    Update badnet_relay.sh

--- BADNET/badnet_relay.sh
+++ BADNET/badnet_relay.sh
@@ -5,13 +5,13 @@ aclocal
 autoconf
 autoheader
 automake
-mkdir -p /usr/local/BADNet-relay/lib/keys
-cp -r src/SC/ /usr/local/BADNet-relay
+mkdir -p /usr/local/BADNET-V3-relay/lib/keys
+cp -r src/SC/ /usr/local/BADNET-V3-relay
 cd ..
-cp BADNet.relay /usr/local/BADNet-relay
-mkdir -p /usr/local/BADNet-relay/log
-cd /usr/local/BADNet-relay/log/
+cp BADNET.relay /usr/local/BADNET-V3-relay
+mkdir -p /usr/local/BADNET-V3-relay/log
+cd /usr/local/BADNET-V3-relay/log/
 touch notice.log info.log debug.log blockchain.log
-cd /usr/local/BADNet-relay/SC
-python3 configure.py relay
+cd /usr/local/BADNET-V3-relay/SC
+python3 configure.py
 cd $path
