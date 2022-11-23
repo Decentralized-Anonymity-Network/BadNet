c1b9877410d67b6d0f237ad9ad6d275a80f50dde
hhyyang <87637701+hhyyang@users.noreply.github.com>
2022-11-23 19:52:32 +0800

    Update configure.py

--- BADNET/src/SC/configure.py
+++ BADNET/src/SC/configure.py
@@ -6,13 +6,13 @@ from eth_account import Account
 from web3 import Web3, HTTPProvider
 import contract_abi
 
-contract_address = "0xe7BC117CE3439e32549115f049d3D17A17EC03c8"
-web3 = Web3(HTTPProvider("https://goerli.infura.io/v3/67631e6ab6fc44e49915da74fd957740"))
+
+contract_address = "0xD6bfD430134759a0deebF58da785c116B524A968"
+web3 = Web3(HTTPProvider("https://sepolia.infura.io/v3/67631e6ab6fc44e49915da74fd957740"))
 contract_instance = web3.eth.contract(address=contract_address, abi=contract_abi.abi)
-path = '/usr/local/BADNET-V3-relay/'
 
 
-def relayCreateAccount(path):
+def create_account(path):
     secp_k = generate_key()
     wallet_private_key = binascii.hexlify(secp_k.secret).decode('utf-8')
     public_key = secp_k.public_key.format(True)
@@ -28,12 +28,15 @@ def relayCreateAccount(path):
     print(wallet_addr)
 
 
-def relayGetUploadPeriod():
-    return contract_instance.functions.relay_get_upload_period().call({'from': account.wallet_addr})
+def relay_get_upload_period():
+    return contract_instance.functions.relay_get_upload_period().call()
 
 
 if __name__ == '__main__':
-    relayCreateAccount(path + 'SC/account.py')
-    RelayUploadPeriod = relayGetUploadPeriod()
+    path = '/usr/local/BADNET-V3-relay/'
+    create_account(path + 'SC/account.py')
+    """
+    RelayUploadPeriod = relay_get_upload_period()
     with open(path + 'BADNET.relay', mode='a') as filename:
         filename.write('\n' + 'RelayUploadPeriod ' + str(RelayUploadPeriod))
+    """
