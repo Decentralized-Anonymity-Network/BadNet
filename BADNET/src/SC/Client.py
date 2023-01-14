3abf9ff246398b2eb0dd31e7fe75546cd382201b
hhyyang <87637701+hhyyang@users.noreply.github.com>
2023-01-14 15:05:18 +0800

    Update Client.py

--- BADNET/src/SC/Client.py
+++ BADNET/src/SC/Client.py
@@ -6,21 +6,22 @@ import binascii
 from Crypto.Cipher import AES
 from ecies import decrypt
 
-web3 = Web3(HTTPProvider("https://goerli.infura.io/v3/67631e6ab6fc44e49915da74fd957740"))
+web3 = Web3(HTTPProvider("https://sepolia.infura.io/v3/67631e6ab6fc44e49915da74fd957740"))
 contract_instance = web3.eth.contract(address=account.contract_address, abi=contract_abi.abi)
 path = '/usr/local/BADNET-V3-client/'
 
 
 def clientDownloadNSD():
     filePath = path + "lib/NSD"
-    if os.path.exists(filePath):
-        os.remove(filePath)
 
-    bytesLocal = 0
     NSDIndex, _NSD = contract_instance.functions.client_download_NSD().call()
     NSD = binascii.hexlify(_NSD)
-    for relayID in range(0, len(NSDIndex)):
-        if NSDIndex[relayID]:
-            with open(filePath, mode='a') as filename:
-                filename.write(relayID + ' ' + NSD[bytesLocal*64:(bytesLocal+1)*64] + '\n')
-                bytesLocal += 1
+
+    with open(filePath, mode='w') as filename:
+        local = 0
+        for relayID in range(0, len(NSDIndex)):
+            if NSDIndex[relayID]:
+                filename.write(str(relayID+1) + ' ')
+                SRIHash = NSD[local*64:(local+1)*64]
+                filename.write(SRIHash.decode() + '\n')
+                local += 1
