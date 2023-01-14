dffec8388041241b149f460d21deb141cb73e762
hhyyang <87637701+hhyyang@users.noreply.github.com>
2023-01-14 13:38:28 +0800

    Update badnet.sh

--- BADNET/badnet.sh
+++ BADNET/badnet.sh
@@ -5,13 +5,11 @@ aclocal
 autoconf
 autoheader
 automake
-mkdir -p /usr/local/BADNet-client/lib/keys
-cp -r src/SC/ /usr/local/BADNet-client
+mkdir -p /usr/local/BADNET-V3-client/lib/keys
+cp -r src/SC/ /usr/local/BADNET-V3-client
 cd ..
-cp BADNet.client /usr/local/BADNet-client
-mkdir -p /usr/local/BADNet-client/log
-cd /usr/local/BADNet-client/log/
+cp BADNET.client /usr/local/BADNET-V3-client
+mkdir -p /usr/local/BADNET-V3-client/log
+cd /usr/local/BADNET-V3-client/log/
 touch notice.log info.log debug.log
-cd /usr/local/BADNet-client/SC
-python3 configure.py client
 cd $path
